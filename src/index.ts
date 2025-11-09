e623b0f659fa106813e0bcf039cdaaa11b4a9468
surajbkale <surajkalecsit@gmail.com>
2025-11-10 00:19:28 +0530

    signup endpoint add

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import z from "zod";
+import z, { hash } from "zod";
 import bcrypt from "bcrypt";
 import { UserModel } from "./db";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+
+const db_connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log(`Connected to db`);
+  } catch (error) {
+    console.log(`Error connecting to db`);
+    process.exit(1);
+  }
+};
 
 app.post("/api/v1/signup", async (req, res) => {
   const inputzod = z.object({
@@ -16,16 +31,18 @@ app.post("/api/v1/signup", async (req, res) => {
       .string()
       .min(8, { message: "password must be at least 8 characters" })
       .max(16, { message: "password must be atmost 16 characters long" })
-      .regex(/\W/, {
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, {
         message: "password must contain atleast one special character",
       }),
   });
 
   const validInput = inputzod.safeParse(req.body);
-
   if (!validInput.success) {
-    const errorMessage = validInput.error.message;
-    res.status(411).json({ message: "invalid format", error: errorMessage });
+    const errorMessage = validInput.error;
+    res.status(411).json({
+      message: "Invalid format",
+      error: errorMessage,
+    });
     return;
   }
 
@@ -50,11 +67,52 @@ app.post("/api/v1/signup", async (req, res) => {
   }
 });
 
-app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    return;
+  }
+
+  if (user === null) {
+    res.status(401).json({ message: "Invalid Credentials" });
+    return;
+  }
+
+  if (user.password) {
+    try {
+      const hashedPassword = await bcrypt.compare(password, user.password);
+      if (hashedPassword) {
+        if (user._id) {
+          const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET as string,
+            { expiresIn: "7days" }
+          );
+          res.status(200).json({
+            message: "User logged in successfully",
+            token,
+          });
+        }
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    res.status(401).json({ message: "Invalid Credentails" });
+  }
+});
+
 app.post("/api/v1/content", (req, res) => {});
 app.get("/api/v1/content", (req, res) => {});
 app.delete("/api/v1/content", (req, res) => {});
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
 app.post("/api/v1/brain/share", (req, res) => {});
 
+db_connect();
 app.listen(3000);
