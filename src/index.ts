5431904f67647135cac1b72ea939632da888f0c0
surajbkale <surajkalecsit@gmail.com>
2025-11-10 01:37:46 +0530

    configure cors with specific origins and methods

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://syncbrain.bitalchemy.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 const db_connect = async () => {
   try {
